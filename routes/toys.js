b698e10839be9a97caf1cf68501b448e0c2f7b7e
AharoniYovel <102188241+AharoniYovel@users.noreply.github.com>
2022-03-22 23:00:34 +0200

    Add files via upload

---
+++ routes/toys.js
@@ -0,0 +1,111 @@
+const express = require("express");
+const { auth } = require("../middlewares/authUser");
+const { toysModel, validatToy } = require("../models/toysModel");
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+    try {
+        let page = req.query.page || 1;
+        let data = await toysModel.find({}).skip((page - 1) * 10);
+        res.json(data);
+    }
+
+    catch (err) {
+        res.json({ msg: "err in server" }).status(500)
+    }
+})
+
+
+router.get("/search", async (req, res) => {
+    try {
+        let page = req.query.page || 1;
+        let search = req.query.s;
+        let sReg = new RegExp(search, "i");
+
+        let data = await toysModel.find({ $or: [{ name: sReg }, { info: sReg }] }).limit(10).skip((page - 1) * 10);
+        res.json(data);
+    }
+
+    catch (err) {
+        res.json({ msg: "err in server" }).status(500)
+    }
+})
+
+router.get("/cat/:category", async (req, res) => {
+    try {
+
+        let page = req.query.page || 1;
+        let search = req.params.category;
+        let sReg = new RegExp(search, "i");
+        let data = await toysModel.find({ category: sReg }).limit(10).skip((page - 1) * 10);;
+        res.json(data);
+    }
+
+    catch (err) {
+        res.json({ msg: "err in server" }).status(500)
+    }
+})
+
+router.get("/prices", async (req, res) => {
+    try {
+        let min = req.query.min;
+        let max = req.query.max;
+        let data = await toysModel.find({ price: { $lte: max, $gte: min } });
+        res.json(data);
+    }
+    catch (err) {
+        console.log(err);
+    }
+})
+
+
+router.post("/", auth, async (req, res) => {
+    let validBody = validatToy(req.body);
+    if (validBody.error) {
+        return res.json(validBody.error.details);
+    }
+    try {
+        let data = new toysModel(req.body);
+        data.user_id = req.datatoken._id;
+        await data.save();
+        res.json(data).status(201);
+
+    }
+
+    catch (err) {
+        console.log(err);
+    }
+})
+
+router.put("/:editid", auth, async (req, res) => {
+    let validBody = validatToy(req.body);
+    if (validBody.error) {
+        return res.json(validBody.error.details);
+    }
+    try {
+        let editId = req.params.editid;
+        let data = await toysModel.updateOne({ _id: editId, User_id: req.datatoken._id }, req.body);
+        res.json(data).status(200);
+    }
+
+    catch (err) {
+        console.log(err);
+    }
+})
+
+
+router.delete("/:delid", auth, async (req, res) => {
+    try {
+        let delid = req.params.delid;
+        let data = await toysModel.deleteOne({ _id: delid, User_id: req.datatoken._id });
+        res.json(data).status(200);
+    }
+
+    catch (err) {
+        console.log(err);
+    }
+})
+
+
+
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const router = express.Router();
+const { validateUser, userModel, validatLogIn, genToken } = require("../models/userModel");
+const bcrypt = require("bcrypt");
+const { auth } = require("../middlewares/authUser");
+
+
+router.get("/", (req, res) => {
+  res.json({ msg: "Users work" });
+})
+
+router.get("/", auth, (req, res) => {
+
+  res.json({ status: "ok" });
+
+})
+
+router.post("/", async (req, res) => {
+  let validBody = validateUser(req.body);
+  if (validBody.error) {
+    return res.json(validBody.error.details).status(400);
+  }
+  try {
+    let data = new userModel(req.body);
+    data.password = await bcrypt.hash(data.password, 10);
+    await data.save();
+    data.password = "********";
+    res.json(data).status(201);
+
+  }
+  catch (err) {
+    if (err.code == 11000) {
+      return res.json({ msg: "email exsist", code: err.code }).status(400);
+    }
+  }
+})
+
+router.post("/logIn", async (req, res) => {
+  let validBody = validatLogIn(req.body);
+  if (validBody.error) {
+    return res.status(400).json(validBody.error.details);
+  }
+  try {
+    let user = await userModel.findOne({ email: req.body.email });
+    if (!user) {
+      return res.json({msg:"invalid mail"});
+    }
+    let validPas = await bcrypt.compare(req.body.password, user.password)
+    if (!validPas) {
+      return res.json({msg:"invalid password"});
+    }
+    let token = genToken(user._id, user.role);
+    res.json({ token }).status(200);
+  }
+  catch (err) {
+    return res.json(err).status(400);
+  }
+})
+
+
+module.exports = router;
+
